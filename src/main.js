d1cce1ebd52ccf5aafee8c9c5c871d4639463bf5
Adryanssyah <adryansyah434@gmail.com>
2023-11-10 14:59:38 +0700

    .env add

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL
 const app = createApp(App)
 
 app.use(createPinia())
