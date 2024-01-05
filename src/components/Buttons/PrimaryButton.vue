4bec76ce09fff16c2bb752a9b824f9dba8908c24
Adryanssyah <adryansyah434@gmail.com>
2024-01-05 23:56:15 +0700

    fix: make button reusable

---
+++ src/components/Buttons/PrimaryButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    :type="type"
+    @click="$emit('click')"
+    :class="{
+      'text-xs': size === 'extraSmall',
+      'text-sm': size === 'small',
+      'text-base': size === 'base',
+      'text-lg': size === 'large',
+      'text-xl': size === 'extraLarge'
+    }"
+    class="inline-flex items-center px-3 py-2 font-medium text-center text-white bg-yellow-500 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'base'
+    }
+  },
+  emits: ['click']
+}
+</script>
---
+++ src/components/Buttons/SecondaryButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button
+    :type="type"
+    @click="$emit('click')"
+    :class="{
+      'text-xs': size === 'extraSmall',
+      'text-sm': size === 'small',
+      'text-base': size === 'base',
+      'text-lg': size === 'large',
+      'text-xl': size === 'extraLarge'
+    }"
+    class="inline-flex items-center px-3 py-2 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+  >
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'text-base'
+    }
+  },
+  emits: ['click']
+}
+</script>
